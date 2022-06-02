8e1a691d3b29a3cff77186d4b0bc1eddad7b83e8
biels <bielsimon2@gmail.com>
2022-06-02 22:09:42 +0200

    Working with Manifest V2 and "browserAction"

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
-chrome.runtime.onInstalled.addListener(async () => {
-  let url = chrome.runtime.getURL("main.html");
-});
+// chrome.runtime.onInstalled.addListener(async () => {
+//   let url = chrome.runtime.getURL("main.html");
+// });
 
+// Use Manifest V2
 let takeScreenshot = async () => {
-  let tab = await chrome.tabs.query({ active: true, currentWindow: true });
-  let tabId = tab[0].windowId;
-  let screenshot = await chrome.tabs.captureVisibleTab(tabId, { format: "png" });
-  // Open in new tab
-  let newTab = await chrome.tabs.create({ url: screenshot, active: true });
+   chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
+     let newTab = chrome.tabs.create({ url: dataUrl, active: true });
+   })
 }
 
-chrome.action.onClicked.addListener((tab) => {
+chrome.browserAction.onClicked.addListener((tab) => {
   takeScreenshot();
 });
