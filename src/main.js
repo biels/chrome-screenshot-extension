5577183005e30a80d955ec1f21bfc914a61e34f9
biels <bielsimon2@gmail.com>
2022-06-02 21:31:27 +0200

    Code cleaning and checked that it is compatible with manifest v2

--- src/main.js
+++ src/main.js
@@ -8,42 +8,19 @@
 //
 // See https://developer.chrome.com/docs/extensions/reference/events/ for additional details.
 chrome.runtime.onInstalled.addListener(async () => {
-
-  // While we could have used `let url = "hello.html"`, using runtime.getURL is a bit more robust as
-  // it returns a full URL rather than just a path that Chrome needs to be resolved contextually at
-  // runtime.
   let url = chrome.runtime.getURL("main.html");
 
-  // Open a new tab pointing at our page's URL using JavaScript's object initializer shorthand.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#new_notations_in_ecmascript_2015
-  //
-  // Many of the extension platform's APIs are asynchronous and can either take a callback argument
-  // or return a promise. Since we're inside an async function, we can await the resolution of the
-  // promise returned by the tabs.create call. See the following link for more info on async/await.
-  // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
-  // let tab = await chrome.tabs.create({ url });
-
-  // Finally, let's log the ID of the newly created tab using a template literal.
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
-  //
-  // To view this log message, open chrome://extensions, find "Hello, World!", and click the
-  // "service worker" link in the card to open DevTools.
-  // console.log(`Created tab ${tab.id}`);
 });
 
 let takeScreenshot = async () => {
-  console.log(`clicked`);
   let tab = await chrome.tabs.query({ active: true, currentWindow: true });
-  console.log(`active tab`, tab);
   let tabId = tab[0].windowId;
   let screenshot = await chrome.tabs.captureVisibleTab(tabId, { format: "png" });
   // Open in new tab
   let newTab = await chrome.tabs.create({ url: screenshot, active: true });
-  console.log(`screenshot`, screenshot);
 }
 
 chrome.action.onClicked.addListener((tab) => {
-  console.log(`tab`, tab);
   takeScreenshot();
 });
 
