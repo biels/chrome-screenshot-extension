fc84ab250ccfa39727e933c27bcd5591029f6c96
biels <bielsimon2@gmail.com>
2022-06-02 23:17:45 +0200

    Added in-page button to take screenshot

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,2 +1,23 @@
-alert("Hello from page")
-console.log(`Hello from page`)
+let el = document.createElement('div');
+el.innerHTML = 'Take screenshot';
+el.onclick = () => {
+    console.log(`chrome.runtime`, chrome.runtime);
+    chrome.runtime.sendMessage({type: "take-screenshot"}, function(response) {
+        console.log(response.success);
+    });
+}
+let s: CSSStyleDeclaration = el.style;
+s.position = 'fixed';
+s.top = '8px';
+s.right = '8px';
+s.cursor = 'pointer';
+s.padding = '4px 8px';
+s.backgroundColor = 'lightblue';
+s.color = 'black';
+s.fontSize = '16px';
+s.zIndex = '99';
+s.fontFamily = 'sans-serif';
+
+
+console.log(`el`, el, document.body);
+document.body.appendChild(el)
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,26 @@
 //   let url = chrome.runtime.getURL("main.html");
 // });
 
-// Use Manifest V2
 let takeScreenshot = async () => {
-   chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
-     let newTab = chrome.tabs.create({ url: dataUrl, active: true });
-   })
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        let current = tabs[0];
+        chrome.tabs.captureVisibleTab(current.windowId, { format: "png" }, (dataUrl) => {
+            console.log(`dataUrl`, dataUrl);
+            let newTab = chrome.tabs.create({ url: dataUrl, active: true });
+        })
+    });
+
 }
 
 chrome.browserAction.onClicked.addListener((tab) => {
   takeScreenshot();
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+      if (request.type == "take-screenshot"){
+          takeScreenshot();
+          sendResponse({success: true});
+      }
+  }
+);
