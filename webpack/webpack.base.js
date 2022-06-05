ae67330b1dd7642a94d2fadd538a2b21183393c9
biels <bielsimon2@gmail.com>
2022-06-05 22:40:57 +0200

    Added webpack to build extension

---
+++ webpack/webpack.base.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const srcDir = path.join(__dirname, "..", "src");
+
+module.exports = {
+  entry: {
+    background: path.join(srcDir, 'background.ts'),
+    content_script: path.join(srcDir, 'content_script.ts'),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {from: ".", to: ".", context: "public"},
+        {from: "./manifest_v2.json", to: "./manifest.json", context: "manifest"}
+      ],
+      options: {},
+    }),
+  ],
+};
