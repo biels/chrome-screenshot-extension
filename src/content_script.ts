f666a7266047ed58e7ee331f54af5c4f726a8b20
biels <bielsimon2@gmail.com>
2022-06-06 13:32:00 +0200

    Buttons from multiple versions of the extension can now be seen side-by-side on the same page if both versions are installed

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,26 +1,30 @@
-let el = document.createElement('div');
-el.innerHTML = 'Take screenshot';
-el.onclick = () => {
-    console.log(`chrome.runtime`, chrome.runtime);
-    chrome.runtime.sendMessage({type: "take-screenshot"}, function(response) {
-        console.log(response.success);
-    });
+let registerButton = () => {
+    let el = document.createElement('div');
+    el.id = `btn-screenshot-${process.env.MANIFEST_VERSION}`;
+    el.innerHTML = `Take screenshot (${process.env.MANIFEST_VERSION})`;
+    el.onclick = () => {
+        chrome.runtime.sendMessage({type: "take-screenshot"}, response => {
+            console.log(response.success);
+        });
+    }
+    let s: CSSStyleDeclaration = el.style;
+    let buttons = ['v2', 'v3'].map((v) => `btn-screenshot-${v}`).map((id) => document.getElementById(id));
+    let foundButtons = buttons.filter(a => a);
+    let existingButtonHeight = foundButtons.reduce((acc, btn) => acc + (btn.getBoundingClientRect().height || 0), 0);
+    s.position = 'fixed';
+    s.top = `${8 + existingButtonHeight}px`;
+    s.right = '8px';
+    s.cursor = 'pointer';
+    s.padding = '4px 8px';
+    s.backgroundColor = 'lightblue';
+    s.color = 'black';
+    s.fontSize = '16px';
+    s.zIndex = '99';
+    s.fontFamily = 'sans-serif';
+    document.body.appendChild(el)
 }
-let s: CSSStyleDeclaration = el.style;
-s.position = 'fixed';
-s.top = '8px';
-s.right = '8px';
-s.cursor = 'pointer';
-s.padding = '4px 8px';
-s.backgroundColor = 'lightblue';
-s.color = 'black';
-s.fontSize = '16px';
-s.zIndex = '99';
-s.fontFamily = 'sans-serif';
+registerButton();
 
-
-console.log(`el`, el, document.body);
-document.body.appendChild(el)
 // alert(`Hello from ${process.env.MANIFEST_VERSION}`);
 console.log(`Hello from ${process.env.MANIFEST_VERSION}`);
 if(process.env.MANIFEST_VERSION == 'v2'){
