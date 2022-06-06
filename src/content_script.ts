0d2d13aec083a4727be64e2d544863f46f6bb7b7
biels <bielsimon2@gmail.com>
2022-06-06 13:42:21 +0200

    Refactored button ids and updated visual styles

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,22 +1,24 @@
 let registerButton = () => {
     let el = document.createElement('div');
-    el.id = `btn-screenshot-${process.env.MANIFEST_VERSION}`;
-    el.innerHTML = `Take screenshot (${process.env.MANIFEST_VERSION})`;
+    let version = process.env.MANIFEST_VERSION;
+    let getButtonId = (v = version) => `btn-screenshot-${v}`;
+    el.id = getButtonId();
+    el.innerHTML = `Take screenshot (${version})`;
     el.onclick = () => {
         chrome.runtime.sendMessage({type: "take-screenshot"}, response => {
             console.log(response.success);
         });
     }
     let s: CSSStyleDeclaration = el.style;
-    let buttons = ['v2', 'v3'].map((v) => `btn-screenshot-${v}`).map((id) => document.getElementById(id));
+    let buttons = ['v2', 'v3'].map((v) => getButtonId(v)).map((id) => document.getElementById(id));
     let foundButtons = buttons.filter(a => a);
     let existingButtonHeight = foundButtons.reduce((acc, btn) => acc + (btn.getBoundingClientRect().height || 0), 0);
     s.position = 'fixed';
     s.top = `${8 + existingButtonHeight}px`;
     s.right = '8px';
     s.cursor = 'pointer';
-    s.padding = '4px 8px';
-    s.backgroundColor = 'lightblue';
+    s.padding = '6px 8px';
+    s.backgroundColor = version != 'v2' ? '#82DBD8' : '#B3E8E5';
     s.color = 'black';
     s.fontSize = '16px';
     s.zIndex = '99';
